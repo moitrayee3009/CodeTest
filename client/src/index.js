10b5b6ed94cd4a7e14a80f85d0f2024ae8aa00f7
Erik Skogetun <bubas-@hotmail.com>
2022-08-29 14:20:33 +0200

    updated files and deps

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
-import App from "./App_functional";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from "./App";
+//import App from "./backup/App_classbased";
+
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
